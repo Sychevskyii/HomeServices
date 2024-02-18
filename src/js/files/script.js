fae0393459e8daca2685398914e13abd683d4c3a
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-02-18 23:24:55 +0200

    update build markup from JSON

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -33,28 +33,37 @@ function buildblogItem(item) {
 
   blogItemTemplate += `<article class="blog__item item-blog">`;
 
-  item.image
-    ? (blogItemTemplate += `<a href="#" class="item-blog__image">
+  item.img
+    ? (blogItemTemplate += `<a href="${item.url}" class="item-blog__image">
   <img src="${item.img}" alt="Image" class="ibg">
 </a>`)
     : null;
 
+  blogItemTemplate += `<div class="item-blog__date">${item.date}</div>`;
+
+  blogItemTemplate += `<h4 class="item-blog__title">
+    <a href="${item.url}" class="item-blog__link-title"
+      >${item.title}</a
+    >
+  </h4>`;
+
+  item.text
+    ? (blogItemTemplate += `<div class="item-blog__text text">
+  ${item.text}
+  </div>`)
+    : null;
+
+  if (item.tags) {
+    blogItemTemplate += `<div class="item-blog__tags">`;
+
+    for (const tag in item.tags) {
+      blogItemTemplate += `<a href="${item.tags[tag]}" class="item-blog__tag">${tag}</a>`;
+    }
+
+    blogItemTemplate += `</div>`;
+  }
+
   blogItemTemplate += `</article>`;
 
-  blogItemTemplate = `
-    
-    <div class="item-blog__date">${item.date}</div>
-    <h4 class="item-blog__title">
-      <a href="#" class="item-blog__link-title"
-        >${item.title}</a
-      >
-    </h4>
-    <div class="item-blog__text text">
-    ${item.text}
-    </div>
-    <div class="item-blog__tags">
-      <a href="#" class="item-blog__tag">Plumbing</a>
-      <a href="#" class="item-blog__tag">Architecture</a>
-      <a href="#" class="item-blog__tag">Maintenance</a>
-    </div>`;
+  blogItems.insertAdjacentHTML('beforeend', blogItemTemplate);
 }
